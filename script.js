072dd8b19d85871a798ed6c7fc8781781aaf43fc
Game-Hub2 <devid.fitz@ge-mittelkreis.de>
2025-09-03 09:19:35 +0000

    gvcxcff

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.error("Does dis work")
+const canvas = document.getElementById("canvas")
+const gl = canvas.getContext("webgl2")
+
+if (!gl) {
+    error("Your browser does not support WebGL")
+    return
+}
+
+const vertices = [
+    -0.5, -0.5, 0.0,
+    0.5 -0.5, 0.0,
+    -0.5, 0.5, 0.0,
+    0.5, 0.5, 0.0,
+]
+
+const indices = [
+    0, 1, 2,
+    1, 2, 3
+]
+
+const vertexBuffer = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verticies), gl.STATIC_DRAW)
+gl.bindBuffer(gl.ARRAY_BUFFER, null)
+
+const indexBuffer = gl.createBuffer()
+gl.bindBuffer(gl.ELEMENT_BUFFER_ARRAY, indexBuffer)
+gl.bufferData(gl.ELEMENT_BUFFER_ARRAY, Uint16Array(indices), gl.STATIC_DRAW)
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null)
